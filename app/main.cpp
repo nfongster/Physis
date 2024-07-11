4321403a54e5332a55d3bd826255fc4578372b34
nfongster <nsfong79@gmail.com>
2024-07-11 07:41:05 -0700

    Added acceleration and time, and compute kinematic values for system evolution

--- app/main.cpp
+++ app/main.cpp
@@ -5,18 +5,35 @@ int main()
     std::cout << "Welcome to the physics simulator.\n";
     std::cout << "Please provide the starting parameters when prompted to do so.\n";
     
-    float x0, v0, a;
-    std::cout << "(1) Initial position (x): ";
+    float x0, v0, a, t;
+    std::cout << "(1) Initial position (x):\t";
     std::cin >> x0;
 
-    std::cout << "(2) Initial velocity (x): ";
+    std::cout << "(2) Initial velocity (x):\t";
     std::cin >> v0;
 
-    std::cout << "(3) Acceleration (constant, x): ";
+    std::cout << "(3) Acceleration (constant, x):\t";
     std::cin >> a;
 
-    std::cout << "You entered:"
+    std::cout << "(5) Time (s):\t\t\t";
+    std::cin >> t;
+
+    std::cout << "\nYou entered:"
         << "\n\tx0:\t" << x0
         << "\n\tv0:\t" << v0
-        << "\n\ta:\t" << a << '\n';
+        << "\n\ta:\t" << a
+        << "\n\tt:\t" << t << '\n';
+
+    float xf = x0 + v0 * t + 0.5 * a * t * t;
+    float vf = v0 + a * t;
+    float dx = xf - x0;
+    float dv = vf - v0;
+    float vAvg = dv / t;
+
+    std::cout << "\nFinal system parameters:"
+        << "\n\txf:\t" << xf
+        << "\n\tvf:\t" << vf
+        << "\n\tdx:\t" << dx
+        << "\n\tdv:\t" << dv
+        << "\n\tvAvg:\t" << vAvg << '\n';
 }
