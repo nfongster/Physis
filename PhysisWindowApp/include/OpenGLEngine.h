662ef983e19534a036145e5b026c7262f4a0ed45
Nick Fong <38268409+nfongster@users.noreply.github.com>
2024-08-24 11:24:50 -0700

    Add shaders

--- PhysisWindowApp/include/OpenGLEngine.h
+++ PhysisWindowApp/include/OpenGLEngine.h
@@ -1,17 +1,24 @@
 #pragma once
 #include <glew.h>
 #include <glfw3.h>
+#include <sstream>
+#include <fstream>
 #include "Physis.h"
 
 class OpenGLEngine : public EngineBase
 {
 private:
 	GLFWwindow* m_window;
+	unsigned int m_shader;
+
 	unsigned int POS_COORDS = 2;
 	unsigned int NUM_VERTICES = 6;
-
 	unsigned int NUM_TRIANGLES = 4;
 	unsigned int NUM_TRIANGLE_CORNERS = 3;
+
+	std::string ParseShaderFile(const std::string& path);
+	unsigned int BuildShaderProgram(const std::string& vertexSource, const std::string& fragmentSource);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
 
 public:
 	OpenGLEngine(const SystemConfig& sc);
--- PhysisWindowApp/src/OpenGLEngine.cpp
+++ PhysisWindowApp/src/OpenGLEngine.cpp
@@ -1,5 +1,59 @@
 #include "OpenGLEngine.h"
 
+std::string OpenGLEngine::ParseShaderFile(const std::string& path)
+{   // Shader parsing/shading ideas taken from Yan Chernikov
+    std::ifstream stream(path);
+    std::string line;
+    std::stringstream ss;
+
+    while (std::getline(stream, line))
+        ss << line << '\n';
+
+    return ss.str();
+}
+
+unsigned int OpenGLEngine::BuildShaderProgram(const std::string& vertexSource, const std::string& fragmentSource)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexSource);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+unsigned int OpenGLEngine::CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " <<
+            (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
 OpenGLEngine::OpenGLEngine(const SystemConfig& sc) : EngineBase(sc)
 {
 }
@@ -49,6 +103,15 @@ void OpenGLEngine::OnStartup()
     glGenBuffers(1, &ibo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, NUM_TRIANGLES * NUM_TRIANGLE_CORNERS * sizeof(int), indices, GL_STATIC_DRAW);
+
+    std::string vertexShaderSource = ParseShaderFile("C:\\Code\\Physis\\PhysisWindowApp\\resource\\shaders\\VertexShader.shader");
+    std::string fragmentShaderSource = ParseShaderFile("C:\\Code\\Physis\\PhysisWindowApp\\resource\\shaders\\FragmentShader.shader");
+    
+    std::cout << vertexShaderSource << '\n';
+    std::cout << fragmentShaderSource << '\n';
+    
+    m_shader = BuildShaderProgram(vertexShaderSource, fragmentShaderSource);
+    glUseProgram(m_shader);
 }
 
 bool OpenGLEngine::ContinueLoop()
@@ -58,6 +121,7 @@ bool OpenGLEngine::ContinueLoop()
 
 void OpenGLEngine::OnCompletion()
 {
+    glDeleteProgram(m_shader);
     glfwTerminate();
 }
 
