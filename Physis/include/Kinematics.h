9ac6c7a7b975a5e31057bc31ecbaaae05cb5456e
Nick Fong <38268409+nfongster@users.noreply.github.com>
2024-09-04 18:21:33 -0700

    Remove dependence on acceleration in UpdatePosition method

--- Physis/include/Kinematics.h
+++ Physis/include/Kinematics.h
@@ -7,7 +7,7 @@ namespace Kinematics
 	template <typename V>
 	V UpdatePosition(V r0, V v0, V a0, double dt)
 	{
-		return r0 + (v0 * dt) + (a0 * dt * dt * 0.5);
+		return r0 + (v0 * dt);
 	}
 
 	template <typename V>
