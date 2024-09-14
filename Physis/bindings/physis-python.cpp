0b41e43c0306c647d09d43f6c97ec53957b98f1f
Nick Fong <38268409+nfongster@users.noreply.github.com>
2024-09-13 18:05:31 -0700

    Add vec2 and vec3 tests and bindings

--- Physis/bindings/physis-python.cpp
+++ Physis/bindings/physis-python.cpp
@@ -20,4 +20,28 @@ PYBIND11_MODULE(physis, m) {
             {
                 return "(" + std::to_string(v.X) + ")";
             });
+
+    py::class_<Vec2>(m, "Vec2")
+        .def(py::init())
+        .def(py::init<double, double>(), "Vector position")
+        .def("equals", &Vec2::Equals, "assess vector equality",
+            py::arg("other"),
+            py::arg("tolerance") = DEFAULT_TOLERANCE)
+        .def("__repr__",
+            [](const Vec2& v)
+            {
+                return "(" + std::to_string(v.X) + ", " + std::to_string(v.Y) + ")";
+            });
+
+    py::class_<Vec3>(m, "Vec3")
+        .def(py::init())
+        .def(py::init<double, double, double>(), "Vector position")
+        .def("equals", &Vec3::Equals, "assess vector equality",
+            py::arg("other"),
+            py::arg("tolerance") = DEFAULT_TOLERANCE)
+        .def("__repr__",
+            [](const Vec3& v)
+            {
+                return "(" + std::to_string(v.X) + ", " + std::to_string(v.Y) + ", " + std::to_string(v.Z) + ")";
+            });
 }
