f83df1cdf00712ecbb781072aa9d756c04bd6b90
Nick Fong <38268409+nfongster@users.noreply.github.com>
2024-09-18 08:17:54 -0700

    add ability to print stability results

--- PhysisPlotter/stability.py
+++ PhysisPlotter/stability.py
@@ -2,15 +2,46 @@ import physis
 from datetime import timedelta
 import os
 
+results_filename = "results.txt"
 
-print("Running benchmark engine...")
-t_total = 1.1
-dt = 0.01
-scalar = 1
-tc = physis.TimeConfig(t_total, dt, scalar)
-outdir = os.path.join(os.path.dirname(__file__), '')
-render_time_ms = 10
-render_time = timedelta(microseconds = render_time_ms * 1000)
 
-engine = physis.BenchmarkEngine(tc, outdir, render_time)
-engine.run()
+def main():
+    print("Running benchmark engine...")
+    # Time parameters
+    t_total = 1.1
+    dt = 0.001
+    scalar = 1
+    tc = physis.TimeConfig(t_total, dt, scalar)
+
+    # Output directory
+    outdir = os.path.join(os.path.dirname(__file__), '')
+
+    # Rendering time per frame
+    render_time_ms = 10
+    render_time = timedelta(microseconds = render_time_ms * 1000)
+
+    # Build engine
+    engine = physis.BenchmarkEngine(tc, outdir, render_time)
+
+    # Add particles
+    pcount = 100
+    for _ in range(10):
+        engine.add()
+
+    # Run engine
+    engine.run()
+    print("Run complete.  Printing ms per frame...\n")
+
+    # Check if txt file exists
+    expected_filepath = os.path.join(outdir, results_filename)
+    if not os.path.exists(expected_filepath):
+        raise FileNotFoundError(f"File not found: {expected_filepath}")
+    
+    # Print contents of txt file
+    with open(expected_filepath, 'r') as file:
+        for line in file:
+            print(line, end='')
+
+
+if __name__ == "__main__":
+    main()
