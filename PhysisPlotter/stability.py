7db5c8b38df0a5a22259574170d9fd5930353df9
Nick Fong <38268409+nfongster@users.noreply.github.com>
2024-09-18 17:58:55 -0700

    Add plot of stability benchmark results

--- PhysisPlotter/stability.py
+++ PhysisPlotter/stability.py
@@ -1,15 +1,17 @@
 import physis
 from datetime import timedelta
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 results_filename = "results.txt"
 
 
-def main():
+if __name__ == "__main__":
     print("Running benchmark engine...")
     # Time parameters
-    t_total = 1.1
-    dt = 0.001
+    t_total = 10
+    dt = 0.01
     scalar = 1
     tc = physis.TimeConfig(t_total, dt, scalar)
 
@@ -25,23 +27,32 @@ def main():
 
     # Add particles
     pcount = 100
-    for _ in range(10):
+    for _ in range(pcount):
         engine.add()
 
     # Run engine
     engine.run()
-    print("Run complete.  Printing ms per frame...\n")
+    print("Run complete.  Getting ms per frame...")
 
     # Check if txt file exists
     expected_filepath = os.path.join(outdir, results_filename)
     if not os.path.exists(expected_filepath):
         raise FileNotFoundError(f"File not found: {expected_filepath}")
     
-    # Print contents of txt file
+    # Get contents of txt file
+    render_times = []
     with open(expected_filepath, 'r') as file:
         for line in file:
-            print(line, end='')
+            render_times.append(float(line))
 
+    # Make and print array
+    times = np.array(render_times)
+    print("Printing ms per frame...\n")
+    print(times)
 
-if __name__ == "__main__":
-    main()
+    # Plot data (clip the initial frame because it is fast)
+    fig, ax = plt.subplots()
+    x = np.arange(len(times) - 1)
+    y = times[1:]
+    ax.scatter(x, y, vmin=0, vmax=100)
+    plt.show()
