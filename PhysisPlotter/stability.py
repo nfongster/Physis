e7cfde226aa716bdf739b5f6295cd53e4e7c569e
Nick Fong <38268409+nfongster@users.noreply.github.com>
2024-09-19 20:11:22 -0700

    Add loop and refactor so that Plotter consumes Data Aggregator

--- PhysisPlotter/stability.py
+++ PhysisPlotter/stability.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 
 
 class EngineWrapper:
-    def __init__(self, t_total=float, dt=float, scalar=float, render_time=timedelta):
+    def __init__(self, t_total=float, dt=float, scalar=float, render_time=timedelta, outdir=str):
         self.time_config = physis.TimeConfig(t_total, dt, scalar)
-        self.outdir = os.path.join(os.path.dirname(__file__), '')
+        self.outdir = outdir
         # TODO: Delay creation of engine
         self.engine = physis.BenchmarkEngine(self.time_config, self.outdir, render_time)
 
@@ -20,10 +20,11 @@ class EngineWrapper:
         self.engine.run()
 
 
-class DataCollector:
+class DataAggregator:
     def __init__(self, outdir=str):
         results_filename = "results.txt"
         self.expected_filepath = os.path.join(outdir, results_filename)
+        self.times_array = []
 
     def read(self) -> np.array:
         if not os.path.exists(self.expected_filepath):
@@ -34,18 +35,22 @@ class DataCollector:
             for line in file:
                 render_times.append(float(line))
         return np.array(render_times)
+    
+    def save(self, times=np.array) -> None:
+        self.times_array.append(times)
 
 
 class Plotter:
-    def __init__(self, times=np.array):
-        self.times = times
+    def __init__(self, aggregator=DataAggregator):
+        self.aggregator = aggregator
         self.fig, self.ax = plt.subplots()
 
     def plot(self):
         # Clip the initial frame because it is fast
-        x = np.arange(len(self.times) - 1)
-        y = self.times[1:]
-        self.ax.scatter(x, y, vmin=0, vmax=100)
+        for times in self.aggregator.times_array:
+            x = np.arange(len(times) - 1)
+            y = times[1:]
+            self.ax.scatter(x, y, vmin=0, vmax=100)
         plt.show()
 
 
@@ -55,19 +60,22 @@ if __name__ == "__main__":
     dt = 0.01
     scalar = 1
     render_time_ms = 1
-    render_time = timedelta(microseconds = render_time_ms * 1000)
     pcount = 1
+    outdir = os.path.join(os.path.dirname(__file__), '')
+    aggregator = DataAggregator(outdir)
 
-    engine = EngineWrapper(t_total, dt, scalar, render_time)
-    engine.initialize(pcount)
-    engine.run()
-    print("Run complete.  Getting ms per frame...")
+    for dt in [0.01, 0.02, 0.03]:
+        print(f"Executing run dt={dt}...")
+        render_time = timedelta(microseconds = render_time_ms * 1000)
+        engine = EngineWrapper(t_total, dt, scalar, render_time, outdir)
+        engine.initialize(pcount)
+        engine.run()
+        print(f"Run dt={dt} complete.  Getting ms per frame...")
 
-    collector = DataCollector(engine.outdir)
-    times = collector.read()
-    print("Printing ms per frame...\n")
-    print(times)
+        times = aggregator.read()
+        aggregator.save(times)
+        print("Data saved.")
 
-    plotter = Plotter(times)
+    plotter = Plotter(aggregator)
     plotter.plot()
     
