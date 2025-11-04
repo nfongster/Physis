e9a9d405021558c15542dc7c9fc943ddd92734cb
Nick Fong <nsfong79@gmail.com>
2025-11-03 21:28:09 -0800

    Remove benchmark engine

--- Physis/Physis.h
+++ Physis/Physis.h
@@ -3,7 +3,6 @@
 // External client use only!
 
 #include "include\SampleEngine.h"
-#include "include\BenchmarkEngine.h"
 #include "include\TimeConfig.h"
 #include "include\KinematicParameters.h"
 #include "include\Boundary.h"
